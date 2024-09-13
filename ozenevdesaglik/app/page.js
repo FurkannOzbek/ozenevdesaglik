22bbfb607c81e6d8d0b02a5e564cc14ac2ae0c28
FurkannOzbek <furkannozbek@yandex.com>
2024-09-13 10:48:29 +0200

    footer added

--- ozenevdesaglik/app/page.js
+++ ozenevdesaglik/app/page.js
@@ -4,6 +4,7 @@ import React from "react";
 import { Parallax } from "react-parallax";
 import styles from "./page.module.css";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 import { Slider } from "@/components/Slider";
 import { Services } from "@/components/Services";
 
@@ -13,6 +14,14 @@ const Home = () => {
       <Header />
       <Slider />
       <Services />
+      <Parallax
+        bgImage="/images/parallax2.jpg" // Replace with the path to your parallax image
+        strength={1600} // Adjust the parallax strength (depth effect)
+        className={styles.parallaxContainer} // Optional: Add a class for additional styling
+      >
+        <div style={{ height: "400px" }}></div> {/* Adjust height to fit your design needs */}
+      </Parallax>
+      <Footer />
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat lobortis
         venenatis...
